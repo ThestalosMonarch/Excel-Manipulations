9503086eb8be37357aa45de3f287b806f5b8fa8e
lucas barros <lucasbarrosrock@hotmail.com>
2023-05-04 22:42:05 -0300

    Add project files.

---
+++ Excel/Actions/ConvertFromXLSBToXLSX.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.IO;
+
+namespace Excel
+{
+    public class ExcelManipulations
+    {
+        public static string ConvertFromXLSBToXLSX(string filepath)
+        {
+            string strNewPath = "";
+            if (!File.Exists(filepath.Replace("xlsb", "xlsx")))
+            {
+                try
+                {
+                    Microsoft.Office.Interop.Excel.Application excelApplication = new Microsoft.Office.Interop.Excel.Application();
+                    Workbooks workbooks = excelApplication.Workbooks;
+                    // open book in any format
+                    Workbook workbook = workbooks.Open(filepath, XlUpdateLinks.xlUpdateLinksNever, true, Type.Missing, Type.Missing, Type.Missing,
+                        Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                    // save in XlFileFormat.xlExcel12 format which is XLSB
+                    workbook.SaveAs(filepath.Replace("xlsb", "xlsx"), XlFileFormat.xlOpenXMLWorkbook, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                        XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                    // close workbook
+                    workbook.Close(false, Type.Missing, Type.Missing);
+
+                    excelApplication.Quit();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(workbooks);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApplication);
+                    strNewPath = filepath.Replace("xlsb", "xlsx");
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    //foreach (System.Diagnostics.Process proc in System.Diagnostics.Process.GetProcessesByName("EXCEL"))
+                    //{
+                    //    proc.Kill();
+                    //}
+                }
+            }
+            else
+            {
+                strNewPath = filepath.Replace("xlsb", "xlsx");
+            }
+            return strNewPath;
+
+        }
+    }
+}
---
+++ Excel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Data;
+using System.Data.OleDb;
+using System;
+using Microsoft.Office.Interop.Excel;
+
+namespace Excel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string strPath = @"C:\Users\lucas\source\repos\Excel\teste.xlsb";
+            ExcelManipulations.ConvertFromXLSBToXLSX(strPath);
+
+        }
+    }
+}
